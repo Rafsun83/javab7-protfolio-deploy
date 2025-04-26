053c0620bce7bc42eaccf01713ae030b780c7762
Rafsun83 <rafsun7774@gmail.com>
2025-04-26 15:46:56 +0600

    First commit

---
+++ src/main/java/com/javaB7/javaB7/JavaB7Application.java
@@ -0,0 +1,14 @@
+package com.javaB7.javaB7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaB7Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaB7Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaB7/javaB7/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.javaB7.javaB7.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping
+    public String home() {
+        return "index";
+    }
+}
+
+
+
