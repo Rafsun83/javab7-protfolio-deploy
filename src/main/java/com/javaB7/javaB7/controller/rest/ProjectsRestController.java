460acc0d136632bfe78122fa23c95c4d9ca9ef52
Rafsun83 <rafsun7774@gmail.com>
2025-05-12 00:09:37 +0600

    Added swagger to documentation

--- src/main/java/com/javaB7/javaB7/controller/rest/ProjectsRestController.java
+++ src/main/java/com/javaB7/javaB7/controller/rest/ProjectsRestController.java
@@ -3,27 +3,30 @@ package com.javaB7.javaB7.controller.rest;
 import com.javaB7.javaB7.model.domain.Projects;
 import com.javaB7.javaB7.model.dto.CreateProject;
 import com.javaB7.javaB7.service.ProjectService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Tag(name = "Project Resource")
 @RestController
+@RequestMapping("/api/projects")
 public class ProjectsRestController {
 
     @Autowired
     ProjectService projectservice;
 
-    @GetMapping("/api/projects")
+
+    @GetMapping
     public List<Projects> getAllProjects() {
         return projectservice.getAllProjects();
     }
 
-    @PostMapping("/api/projects")
+    @PostMapping
     public Projects createProject(@RequestBody CreateProject createProject) {
         return projectservice.createProject(createProject);
     }
